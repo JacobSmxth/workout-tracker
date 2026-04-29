cbcb44ada8f51b65fe6dd1e8ace929ca09ac49db
Jacob <Jacob.d.smith@live.com>
2026-04-29 11:31:11 -0400

    feat: complete the set class, create the exercise class - Exercise contains a addSet function to add Sets to it. Calculating stat items

---
+++ src/com/jacob/workout/exercise/Exercise.java
@@ -0,0 +1,76 @@
+package com.jacob.workout.exercise;
+
+import com.jacob.workout.set.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Exercise {
+
+    private String name;
+
+    private double totalVolume;
+    private double avgRPE;
+    private int totalReps;
+
+    private List<Set> sets;
+
+    public Exercise(String name) {
+        this.name = name;
+        sets = new ArrayList<>();
+        this.totalVolume = 0;
+        this.totalReps = 0;
+        this.avgRPE = 0;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName() {
+        this.name = name;
+    }
+
+    public void addSet(Set newSet) {
+        int currentLen = this.sets.size(); // Just so I can call once and keep local
+        this.avgRPE *= currentLen; // To get total RPE
+        this.avgRPE += newSet.getRPE(); // Add new RPE value
+        this.avgRPE /= currentLen + 1; // Get new average
+        this.sets.add(newSet);
+        this.totalVolume += newSet.getWeight();
+    }
+
+    public void setSets(List<Set> sets) {
+        for (Set set : sets) {
+            addSet(set);
+        }
+    }
+
+    public List<Set> getSets() {
+        return this.sets;
+    }
+
+    public double getTotalVolume() {
+        return totalVolume;
+    }
+
+    public void setTotalVolume(double totalVolume) {
+        this.totalVolume = totalVolume;
+    }
+
+    public double getAvgRPE() {
+        return avgRPE;
+    }
+
+    public void setAvgRPE(double avgRPE) {
+        this.avgRPE = avgRPE;
+    }
+
+    public int getTotalReps() {
+        return totalReps;
+    }
+
+    public void setTotalReps(int totalReps) {
+        this.totalReps = totalReps;
+    }
+}
--- src/com/jacob/workout/set/Set.java
+++ src/com/jacob/workout/set/Set.java
@@ -4,4 +4,45 @@ public class Set {
     private int reps;
     private double weight;
     private double RPE;
+
+    public Set(int reps, double weight, double RPE) {
+        setReps(reps);
+        setWeight(weight);
+        setRPE(RPE);
+    }
+
+    public double getRPE() {
+        return this.RPE;
+    }
+
+    public void setRPE(double RPE) {
+        if (RPE % .5 != 0) {
+            throw new IllegalArgumentException("RPE must end in .5");
+        } else if (RPE < 0) {
+            throw new IllegalArgumentException("RPE must be greater than zero.");
+        }
+        this.RPE = RPE;
+    }
+
+    public int getReps() {
+        return this.reps;
+    }
+
+    public void setReps(int reps) {
+        if (reps < 0) {
+            throw new IllegalArgumentException("Reps must be greater than zero.");
+        }
+        this.reps = reps;
+    }
+
+    public double getWeight() {
+        return this.weight;
+    }
+
+    public void setWeight(double weight) {
+        if (weight < 0) {
+            throw new IllegalArgumentException("Weight must be greater than zero.");
+        }
+        this.weight = weight;
+    }
 }
